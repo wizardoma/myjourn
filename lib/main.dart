e1d98e04a26c2de9d55df4a61959a88fcc690768
Ibekason Alexander <alibekason@gmail.com>
2021-03-17 16:36:07 +0100

    feat: Add dummy journals to journal_provider.dart for test

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfrontend/providers/journal_provider.dart';
 import 'package:flutterfrontend/screens/home/home_screen.dart';
 import 'package:flutterfrontend/screens/journal/new_journal_screen.dart';
 import 'package:flutterfrontend/screens/search/search_screen.dart';
 import 'package:flutterfrontend/screens/settings/settings_screen.dart';
 import 'package:flutterfrontend/themes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,17 +14,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "My Journal",
-      debugShowCheckedModeBanner: false,
-      theme: theme,
-      routes: {
-        NewJournalScreen.routeName: (context) => NewJournalScreen(),
-        HomeScreen.routeName: (context) => HomeScreen(),
-        SearchScreen.routeName: (context) => SearchScreen(),
-        SettingsScreen.routeName: (context) => SettingsScreen(),
-      },
-      home: HomeScreen(),
+    return ChangeNotifierProvider.value(
+      value: JournalProvider(),
+      child: MaterialApp(
+        title: "My Journal",
+        debugShowCheckedModeBanner: false,
+        theme: theme,
+        routes: {
+          NewJournalScreen.routeName: (context) => NewJournalScreen(),
+          HomeScreen.routeName: (context) => HomeScreen(),
+          SearchScreen.routeName: (context) => SearchScreen(),
+          SettingsScreen.routeName: (context) => SettingsScreen(),
+        },
+        home: HomeScreen(),
+      ),
     );
   }
 }
--- lib/providers/journal_provider.dart
+++ lib/providers/journal_provider.dart
@@ -2,8 +2,31 @@ import 'package:flutter/widgets.dart';
 import 'package:flutterfrontend/models/journal.dart';
 
 class JournalProvider with ChangeNotifier {
-
-  List<Journal> _journals = [];
+  List<Journal> _journals = [
+    Journal("1", "Tomorrow will be awesome for me", DateTime.now()),
+    Journal("2", "I have started serious work on Dart and Flutter",
+        DateTime.now().add(Duration(days: 10))),
+    Journal("3", "Am so grateful for everything ",
+        DateTime.now().add(Duration(minutes: 10))),
+    Journal("4", "How are you doing? Ill be back to you",
+        DateTime.now().add(Duration(days: 14))),
+    Journal("5", "I am on track on making my first million naira",
+        DateTime.now().add(Duration(hours: 10))),
+    Journal("6", "I am really grateful for life and everything it gave me",
+        DateTime.now().add(Duration(hours: 35))),
+    Journal("7", "I;ve not been able to meet up with certain things so far",
+        DateTime.now().add(Duration(minutes: 34))),
+    Journal(
+        "8",
+        "The more i work at Shoprite, they more i realize its not for me",
+        DateTime.now().add(Duration(days: 40, hours: 3))),
+    Journal("9", "I am going to be a very great software developer",
+        DateTime.now().add(Duration(minutes: 10))),
+    Journal(
+        "10",
+        "I am going to venture fully into being a professional software developer",
+        DateTime.now().add(Duration(seconds: 48))),
+  ];
 
   get journals {
     return [..._journals];
@@ -13,20 +36,17 @@ class JournalProvider with ChangeNotifier {
     this._journals.add(journal);
   }
 
-  void deleteJournal(String id){
+  void deleteJournal(String id) {
     this._journals.removeWhere((element) => element.id == id);
-
   }
 
-  void editJournal(String id, String body){
+  void editJournal(String id, String body) {
     Journal journal = this._journals.firstWhere((element) => element.id == id);
-    if (journal == null){
+    if (journal == null) {
       return;
     }
 
     journal.body = body;
     addJournal(journal);
-
   }
-
 }
