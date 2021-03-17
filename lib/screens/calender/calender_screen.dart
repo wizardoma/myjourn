14d310d783048b1b332246df3b0b690f39d3e658
Ibekason Alexander <alibekason@gmail.com>
2021-03-17 15:40:14 +0100

    feat: add homescreen with tablayouts

---
+++ lib/screens/calender/calender_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CalenderScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text("Calenders coming soon"),
+    );
+  }
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfrontend/screens/calender/calender_screen.dart';
+import 'package:flutterfrontend/screens/home/journal_list.dart';
+import 'package:flutterfrontend/screens/more/more_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = "/home";
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("MYJOURN"),
+          ),
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          actions: [
+            DropdownButton(icon: Icon(Icons.more_vert), items: [
+              DropdownMenuItem(
+                child: Text("Search"),
+                onTap: () {},
+              )
+            ])
+          ],
+          bottom: TabBar(
+            tabs: [
+              Padding(padding: EdgeInsets.symmetric(vertical: 13), child: Text("HOME")),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 13),
+                child: Text("CALENDER"),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 13),
+                child: Text("MORE"),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [JournalList(), CalenderScreen(), MoreScreen()],
+        ),
+      ),
+    );
   }
 }
