659cf238e32c2d384bddc48f944321dc8687f92f
Ibekason Alexander <alibekason@gmail.com>
2021-03-22 09:22:17 +0100

    refractor: Delete dummy journals

--- lib/providers/journal_provider.dart
+++ lib/providers/journal_provider.dart
@@ -3,37 +3,6 @@ import 'package:flutterfrontend/models/journal.dart';
 import '../services/repository/journal_repository.dart';
 
 class JournalProvider with ChangeNotifier {
-  List<Journal> _journals = [
-    Journal(
-        1,
-        "Tomorrow will be awesome for me, "
-            "I can't even begin to think how i would have learned so much in programming to be this good."
-            "I know am gonna stand out in this career. I just gotta keep pushing everyday. "
-            "I will never Stop no matter what happens. This is the heart and mentality of a warrior",
-        DateTime.now()),
-    Journal(2, "I have started serious work on Dart and Flutter",
-        DateTime.now().subtract(Duration(days: 1))),
-    Journal(3, "Am so grateful for everything ",
-        DateTime.now().subtract(Duration(minutes: 10))),
-    Journal(4, "How are you doing? Ill be back to you",
-        DateTime.now().subtract(Duration(days: 14))),
-    Journal(5, "I am on track on making my first million naira",
-        DateTime.now().subtract(Duration(hours: 24))),
-    Journal(6, "I am really grateful for life and everything it gave me",
-        DateTime.now().subtract(Duration(hours: 35))),
-    Journal(7, "I;ve not been able to meet up with certain things so far",
-        DateTime.now().subtract(Duration(minutes: 34))),
-    Journal(
-        8,
-        "The more i work at Shoprite, they more i realize its not for me",
-        DateTime.now().subtract(Duration(days: 10, hours: 3))),
-    Journal(9, "I am going to be a very great software developer",
-        DateTime.now().subtract(Duration(minutes: 10))),
-    Journal(
-        10,
-        "I am going to venture fully into being a professional software developer",
-        DateTime.now().subtract(Duration(seconds: 48))),
-  ];
 
   List<Journal> _dbJournals = [];
 
@@ -56,7 +25,6 @@ class JournalProvider with ChangeNotifier {
   Future<bool> addJournal(Journal journal) async{
     int result = await JournalRepository.instance.insert(Journal.toMap(journal));
     await fetchJournals();
-    print("result of saving ${Journal.toMap(journal)} was : $result");
     notifyListeners();
 
     return databaseOpWasSuccessful(result);
@@ -71,7 +39,6 @@ class JournalProvider with ChangeNotifier {
 
   Future<bool> editJournal(Journal journal) async{
     int result = await JournalRepository.instance.update(Journal.toMap(journal));
-    print("result of updating is: $result");
     await fetchJournals();
     notifyListeners();
     return databaseOpWasSuccessful(result);
