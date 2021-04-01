53547b8a3f832297825d24c457bc1fb16e12af57
Ibekason Alexander <alibekason@gmail.com>
2021-04-01 21:44:03 +0100

    Add Search bloc for journal

--- app/lib/bloc/search_journal_bloc.dart
+++ app/lib/bloc/search_journal_bloc.dart
@@ -1,25 +1,42 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutterfrontend/bloc/journal_bloc.dart';
 import 'package:flutterfrontend/bloc/journal_events.dart';
 import 'package:flutterfrontend/bloc/journal_state.dart';
+import 'package:flutterfrontend/models/journal.dart';
 import 'package:flutterfrontend/services/repository/journal_repository.dart';
 
 class SearchJournalBloc extends Bloc<JournalEvents, JournalState> {
-  final JournalBloc _journalBloc;
+  final JournalRepository _repository;
 
-  SearchJournalBloc(JournalBloc journalBloc) : _journalBloc = JournalBloc(), super(SearchEmpty());
+  SearchJournalBloc(JournalRepository repository) : _repository = repository?? JournalRepository.instance, super(SearchEmpty());
+
+  List<Journal> _journals = [];
 
   @override
   Stream<JournalState> mapEventToState(JournalEvents event) async*{
-    // TODO: implement mapEventToState
     if (event is SearchJournalEvent){
-      yield searchJournal(event);
+      yield await searchJournal(event);
     }
   }
 
-  JournalState searchJournal(SearchJournalEvent event) {
+  Future<JournalState> searchJournal(SearchJournalEvent event)async {
       try {
-        _repository.
+        List<Journal> result;
+        if (_journals.length == 0) {
+          result = (await _repository.all()).map((e) =>
+              Journal.fromNewMap(e)).toList().where((element) =>
+              element.body.contains(event.query));
+        }
+        else {
+          result = _journals.where((element) =>
+              element.body.contains(event.query));
+        }
+        if (result.length > 0) {
+          return SearchFound(result);
+        }
+        return SearchEmpty();
+      }
+      catch (e) {
+        return SearchEmpty();
       }
   }
 
