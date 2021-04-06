d1b00886070227f6b6712b6d1ff6c345710aa32f
Ibekason Alexander <alibekason@gmail.com>
2021-04-06 17:34:55 +0100

    refractor

--- app/lib/screens/auth/signin_textfield_widget.dart
+++ app/lib/screens/auth/signin_textfield_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignInTextField extends StatefulWidget {
@@ -5,28 +6,49 @@ class SignInTextField extends StatefulWidget {
   final Function(String) validator;
   final TextEditingController textEditingController;
 
-  const SignInTextField(this.labelText, this.validator, this.textEditingController);
+  const SignInTextField(
+      this.labelText, this.validator, this.textEditingController);
 
   @override
   _SignInTextFieldState createState() => _SignInTextFieldState();
 }
 
 class _SignInTextFieldState extends State<SignInTextField> {
+  FocusNode _focusNode;
+
+  @override
+  void initState() {
+    _focusNode = FocusNode();
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.symmetric(vertical: 15),
-
       child: TextFormField(
-        keyboardType: widget.labelText == "Email"? TextInputType.emailAddress : TextInputType.text,
+        style: Theme.of(context).textTheme.headline4.copyWith(fontSize: 15),
+        keyboardType: widget.labelText == "Email"
+            ? TextInputType.emailAddress
+            : TextInputType.text,
         controller: widget.textEditingController,
-        decoration: InputDecoration(
-            hintText: widget.labelText,
-            labelText: widget.labelText
-        ),
-        validator:(s) => widget.validator(s),
-      ),
 
+        decoration: InputDecoration(
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade600)
+          ),
+            focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.grey.shade600)
+            ),border: UnderlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey.shade600)
+        ),
+//            hintText: widget.labelText,
+            labelText: widget.labelText,
+            labelStyle:
+                Theme.of(context).textTheme.headline2.copyWith(fontSize: 15)),
+        validator: (s) => widget.validator(s),
+      ),
     );
   }
 }
