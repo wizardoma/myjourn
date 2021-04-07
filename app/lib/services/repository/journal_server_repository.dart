71734a991b9e79b76a100d053e5734c23b685aeb
Ibekason Alexander <alibekason@gmail.com>
2021-04-07 22:13:37 +0100

    add journal backend crud repository

---
+++ app/lib/services/repository/journal_server_repository.dart
@@ -0,0 +1,58 @@
+import 'package:dio/dio.dart';
+import 'package:flutterfrontend/services/repository/jsonresponse.dart';
+import 'package:flutterfrontend/services/repository/server_const.dart';
+
+class JournalServerRepository {
+  static final Dio _dio = Dio();
+
+  Future<JsonResponse> save(FormData data, Map<String, dynamic> headers) async {
+    Response response;
+    try {
+      response = await _dio.post(ServerConstants.journalsUrl,
+          data: data, options: Options(headers: headers));
+      return JsonResponse.fromResponse(response);
+    } on DioError catch (e) {
+      print(e);
+      return JsonResponse.fromResponse(response);
+    }
+  }
+
+  Future<JsonResponse> getAll(Map<String, dynamic> headers) async {
+    Response response;
+    try {
+      response = await _dio.get(ServerConstants.journalsUrl,
+          options: Options(headers: headers));
+      return JsonResponse.fromResponse(response);
+    } on DioError catch (e) {
+      print(e);
+      return JsonResponse.fromResponse(response);
+    }
+  }
+
+  Future<JsonResponse> edit(
+      int dbId, FormData request, Map<String, dynamic> headers) async {
+    Response response;
+    try {
+      response = await _dio.patch("${ServerConstants.journalsUrl}/$dbId",
+          data: request, options: Options(headers: headers));
+
+      return JsonResponse.fromResponse(response);
+    } on DioError catch (e) {
+      print(e);
+      return JsonResponse.fromResponse(response);
+    }
+  }
+
+  Future<JsonResponse> delete(
+      int dbId, FormData data, Map<String, dynamic> headers) async {
+    Response response;
+    try {
+      response = await _dio.delete("${ServerConstants.journalsUrl}/$dbId",
+          data: data, options: Options(headers: headers));
+      return JsonResponse.fromResponse(response);
+    } on DioError catch (e) {
+      print(e);
+      return JsonResponse.fromResponse(response);
+    }
+  }
+}
